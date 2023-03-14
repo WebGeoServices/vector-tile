05f8fa6904b963e98fb4df9ff38073ed7705cdec
Emmanuel Peralta <eperalta@webgeoservices.com>
2023-03-14 11:29:39 +0100

    Adds feature.getValue to the benchmark.

--- bench/run.cpp
+++ bench/run.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
 #include <chrono>
-#include <mapbox/vector_tile.hpp>
 #include <fstream>
+#include <iostream>
+#include <mapbox/vector_tile.hpp>
 
 std::size_t feature_count = 0;
 
@@ -14,43 +14,47 @@ static void decode_entire_tile(std::string const& buffer) {
             std::cout << "Layer '" << name << "' (empty)\n";
             continue;
         }
-        for (std::size_t i=0;i<num_features;++i) {
-            auto const feature = mapbox::vector_tile::feature(layer.getFeature(i),layer);
+        for (std::size_t i = 0; i < num_features; ++i) {
+            auto const feature = mapbox::vector_tile::feature(layer.getFeature(i), layer);
             auto const& feature_id = feature.getID();
             if (feature_id.is<mapbox::feature::null_value_t>()) {
                 throw std::runtime_error("Hit unexpected error decoding feature");
             }
             auto props = feature.getProperties();
-            mapbox::vector_tile::points_arrays_type geom = feature.getGeometries<mapbox::vector_tile::points_arrays_type>(1.0);
+
+            mapbox::feature::value featureClass = feature.getValue("class");
+
+            mapbox::vector_tile::points_arrays_type geom =
+                feature.getGeometries<mapbox::vector_tile::points_arrays_type>(1.0);
             ++feature_count;
         }
     }
 }
 
 static void run_bench(std::vector<std::string> const& tiles, std::size_t iterations) {
-
-    for (std::size_t i=0;i<iterations;++i) {
-        for (auto const& tile: tiles) {
+    for (std::size_t i = 0; i < iterations; ++i) {
+        for (auto const& tile : tiles) {
             decode_entire_tile(tile);
         }
     }
 }
 
-template <typename T>
-using milliseconds = std::chrono::duration<T, std::milli>;
+template <typename T> using milliseconds = std::chrono::duration<T, std::milli>;
 
-int main(/*int argc, char* const argv[]*/) {
+int main() {
+    std::size_t totalFeatureCount = 80770;
+
     try {
         std::vector<std::string> tiles;
-        for (std::size_t x=4680;x<=4693;++x) {
-            for (std::size_t y=6260;y<=6274;++y) {
-                std::string path = "bench/mvt-bench-fixtures/fixtures/14-" + std::to_string(x) + "-" + std::to_string(y) + ".mvt";
-                std::ifstream stream(path.c_str(),std::ios_base::in|std::ios_base::binary);
-                if (!stream.is_open())
-                {
+        for (std::size_t x = 4680; x <= 4693; ++x) {
+            for (std::size_t y = 6260; y <= 6274; ++y) {
+                std::string path =
+                    "bench/mvt-bench-fixtures/fixtures/14-" + std::to_string(x) + "-" + std::to_string(y) + ".mvt";
+                std::ifstream stream(path.c_str(), std::ios_base::in | std::ios_base::binary);
+                if (!stream.is_open()) {
                     throw std::runtime_error("could not open: '" + path + "'");
                 }
-                std::string message(std::istreambuf_iterator<char>(stream.rdbuf()),(std::istreambuf_iterator<char>()));
+                std::string message(std::istreambuf_iterator<char>(stream.rdbuf()), (std::istreambuf_iterator<char>()));
                 stream.close();
                 tiles.emplace_back(message);
             }
@@ -58,15 +62,18 @@ int main(/*int argc, char* const argv[]*/) {
         std::clog << "decoding " << tiles.size() << " tiles\n";
         std::clog << "warming up...\n";
         // warmup
-        run_bench(tiles,1);
+        run_bench(tiles, 1);
         std::clog << "running bench...\n";
         // now actually decode all tiles for N iterations
         auto t1 = std::chrono::high_resolution_clock::now();
-        run_bench(tiles,100);
+        std::size_t iterations = 100;
+        run_bench(tiles, iterations);
         auto t2 = std::chrono::high_resolution_clock::now();
         auto elapsed = milliseconds<double>(t2 - t1).count();
-        if (feature_count != 8157770) {
-            std::clog << "Warning expected feature_count of 8157770, was: " << feature_count << "\n";
+        std::size_t featureCountBench = totalFeatureCount * (iterations + 1);
+
+        if (feature_count != featureCountBench) {
+            std::clog << "Warning expected feature_count of "<< featureCountBench <<", was: " << feature_count << "\n";
         }
         std::clog << "elapsed: " << std::fixed << elapsed << " ms\n";
     } catch (std::exception const& ex) {
